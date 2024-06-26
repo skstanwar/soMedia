3c544fbe5af1eac44ca504c88a939dd3f8b1e15b
Sanjay Kumar Saini <kumarsaini0098@gmail.com>
2024-06-26 18:09:50 +0000

    fix comment and post structure in admin panel

--- chat/admin.py
+++ chat/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 
 from .models import Comment, Post
 
-admin.site.register(Comment)
-admin.site.register(Post)
+class _comment_structure(admin.ModelAdmin):
+    list_display=("user" , "text","comment_date" )
+
+class _Post_structure(admin.ModelAdmin):
+    list_display=("user" , "text","picture" )
+admin.site.register(Comment , _comment_structure)
+admin.site.register(Post , _Post_structure)
